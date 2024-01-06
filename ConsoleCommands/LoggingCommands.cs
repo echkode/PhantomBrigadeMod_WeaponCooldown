ec6e11dd7b1ca9d37679fe64be793833cfaed55d
EchKode <48565771+echkode@users.noreply.github.com>
2024-01-06 17:40:43 +0000

    Remove redundant prefix from console command names

--- ConsoleCommands/LoggingCommands.cs
+++ ConsoleCommands/LoggingCommands.cs
@@ -11,49 +11,49 @@ namespace EchKode.PBMods.WeaponCooldown
 {
 	static partial class ConsoleCommands
 	{
-		[ConsoleCommand("log", "toggle-act-lockout-stat", "Toggle logging access to activation lockout stat")]
+		[ConsoleCommand("log", "act-lockout-stat", "Toggle logging access to activation lockout stat")]
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Console command registered through reflection")]
 		static void ToggleActivationLockoutStat()
 		{
 			FlipLoggingToggle(nameof(LoggingToggles.ActivationLockoutStat));
 		}
 
-		[ConsoleCommand("log", "toggle-bt-injection", "Toggle logging injection of BTNodes into combat behavior trees")]
+		[ConsoleCommand("log", "bt-injection", "Toggle logging injection of BTNodes into combat behavior trees")]
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Console command registered through reflection")]
 		static void ToggleBTInjection()
 		{
 			FlipLoggingToggle(nameof(LoggingToggles.BTInjection));
 		}
 
-		[ConsoleCommand("log", "toggle-btnode-updates", "Toggle logging in OnUpdate() of custom BTNodes")]
+		[ConsoleCommand("log", "btnode-updates", "Toggle logging in OnUpdate() of custom BTNodes")]
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Console command registered through reflection")]
 		static void ToggleBTUpdates()
 		{
 			FlipLoggingToggle(nameof(LoggingToggles.BTUpdates));
 		}
 
-		[ConsoleCommand("log", "toggle-action-widget-depth", "Toggle logging of algorithm to fix depth of action widgets on combat timeline")]
+		[ConsoleCommand("log", "action-widget-depth", "Toggle logging of algorithm to fix depth of action widgets on combat timeline")]
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Console command registered through reflection")]
 		static void ToggleActionWidgetDepths()
 		{
 			FlipLoggingToggle(nameof(LoggingToggles.ActionWidgetDepths));
 		}
 
-		[ConsoleCommand("log", "toggle-action-widget-depth-verbose", "Toggle verbose logging of algorithm to fix depth of action widgets on combat timeline")]
+		[ConsoleCommand("log", "action-widget-depth-verbose", "Toggle verbose logging of algorithm to fix depth of action widgets on combat timeline")]
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Console command registered through reflection")]
 		static void ToggleActionWidgetDepthsVerbose()
 		{
 			FlipLoggingToggle(nameof(LoggingToggles.ActionWidgetDepthsVerbose));
 		}
 
-		[ConsoleCommand("log", "toggle-action-drag", "Toggle logging when dragging action widgets on combat timeline")]
+		[ConsoleCommand("log", "action-drag", "Toggle logging when dragging action widgets on combat timeline")]
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Console command registered through reflection")]
 		static void ToggleActionDrag()
 		{
 			FlipLoggingToggle(nameof(LoggingToggles.ActionDrag));
 		}
 
-		[ConsoleCommand("log", "toggle-ai-invoke", "Toggle logging of AI behavior")]
+		[ConsoleCommand("log", "ai-invoke", "Toggle logging of AI behavior")]
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Console command registered through reflection")]
 		static void ToggleAIBehaviorInvoke()
 		{
